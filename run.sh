ea6b3905179c9c75c18851f5795146ddd7242273
David Apolinar <david.apolinar@hotmail.com>
2022-11-10 18:12:39 -0500

    initial commit fio

---
+++ run.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+FILE=$(find /opt/config/*.ini)
+DIR="/mnt/azurefiles"
+echo "Using fio config file: $FILE"
+if [[ ! -f "$FILE" ]]; then 
+    echo "Configuration file does not exist"
+    exit 1
+fi
+
+if [[ ! -d "$DIR" ]]; then 
+    echo "$DIR does not exist."    
+    echo "Check that Azure Files has been mounted by running mount | grep -i azurefiles"
+    exit 1
+else
+    echo "Found $DIR"
+    echo "NOTE: $DIR may be a local drive and not an actual mounted Azure Files volume" 
+fi
+
+[ -z "$FIO_CFG_FILE" ] && echo "FIO_CFG_FILE variable is required." && exit 1;
+[ -z "$RUNTIME" ] && echo "No RUNTIME was found. Setting default of 60 seconds" && RUNTIME="${RUNTIME:=30}";
+[ -z "$OUTPUT" ] && echo "No OUTPUT was specified, using default tmp directory" && OUTPUT="${OUTPUT:=/tmp/fio.output}";
+
+echo "Running fio benchmark using the following file $FIO_CFG_FILE with a runtime of $RUNTIME"
+
+fio $FILE --output=$OUTPUT --runtime $RUNTIME 
