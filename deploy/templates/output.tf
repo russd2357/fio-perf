fce211cdf23d0707be59ef0d3b20de11c2eb254a
David Apolinar <david.apolinar@hotmail.com>
2023-03-08 18:31:09 -0500

    updated sa output

--- deploy/templates/output.tf
+++ deploy/templates/output.tf
@@ -1,5 +1,5 @@
 output "storage_account_name" {
-  value = var.nfs_share_enabled ? null : azurerm_storage_account.storage_account.name
+  value = azurerm_storage_account.storage_account.name
   description = "Storage account name"
 }
 
